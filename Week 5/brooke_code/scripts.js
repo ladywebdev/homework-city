4ef40cbbe2f280ae14a483942d1fcc7aa13c777e
Brooke <ladywebdev@outlook.com>
2018-02-11 13:57:20 -0500

    Week 5 Changes

--- Week 5/brooke_code/scripts.js
+++ Week 5/brooke_code/scripts.js
@@ -1,34 +1,21 @@
 // Javascript File
 //Brooke Guy - Homework Week 5
 
+
+//ASSIGNMENT INSTRUCTIONS
 // Call ```$.val()``` on inputs to get the string value of your user's input          
-
 //  Store user input in ```var city``` 
-
-// | Store user input in ```var city```  
-
-// Use $.on(submit) or ```$.click``` to figure out when the user clicks the "submit" button 
-                                                                                                                      
+// Use $.on(submit) or ```$.click``` to figure out when the user clicks the "submit" button                                                                                                                  
 //Create ```if / else if / else``` conditionals to control the flow of your application           
-
-//Write at least six different lines of pseudocode and display them inline as JavaScript comments     
-                                                                                                       
-//Prevent a form submission using the ```event.preventDefault()``` function            
-                                                                                                                         
-//Use the ```$.ready()``` handler to delay your code from executing until all DOM assets have been loaded 
-                                                                                                       
-//Get the first element from an attribute name using ```$.attr()```                             
-                                                                                                               
-//Use the ```||``` operator in your conditionals to allow for multiple string values to execute ```if/else if``` statement code   
-                                                                               
-//If a user submits "New York" or "New York City" or "NYC" make the background of the page nyc.jpg             
-                                                                                                
-//If a user submits "San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg            
-                                                                                                     
-//If a user submits "Los Angeles" or "LA" or "LAX" make the background of the page la.jpg    
-                                                                                                                  
-//If a user submits "Austin" or "ATX" make the background of the page austin.jpg     
-                                                                                                                     
+//Write at least six different lines of pseudocode and display them inline as JavaScript comments                                                                                                      
+//Prevent a form submission using the ```event.preventDefault()``` function                                                                                                                              
+//Use the ```$.ready()``` handler to delay your code from executing until all DOM assets have been loaded                                                                                                  
+//Get the first element from an attribute name using ```$.attr()```                                                                                                                                       
+//Use the ```||``` operator in your conditionals to allow for multiple string values to execute ```if/else if``` statement code                                                                        
+//If a user submits "New York" or "New York City" or "NYC" make the background of the page nyc.jpg                                                                                            
+//If a user submits "San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg                                                                                                    
+//If a user submits "Los Angeles" or "LA" or "LAX" make the background of the page la.jpg                                                                                                           
+//If a user submits "Austin" or "ATX" make the background of the page austin.jpg                                                                                                                    
 //If a user submits "Sydney" or "SYD" make the background of the page sydney.jpg   
 
 
@@ -37,8 +24,8 @@ $(document).ready(function() {
 	console.log( 'ready!');
 
 //Get the first element from an attribute name using ```$.attr()```
-$( "img" ).attr({
-	src="/images/nyc.jpg",
+$("img" ).attr({
+  src:"/images/nyc.jpg",
   title: "city photo",
   alt: "photo of _"
 });
@@ -46,38 +33,54 @@ $( "img" ).attr({
 $( "div" ).text( $( "img" ).attr( "alt" ) ); 
 
 // | Store user input in ```var city```  
-	var cityNewYork = ["New York", "New York City", "NYC"];
-	var citySanFran = ["San Francisco", "SF", "Bay Area"]
-	var cityLosAng = ["Los Angeles", "LA", "LAX"]
-	var cityAustin = ["Austin", "ATX"]
-	var citySydney = ["Sydney", "SYD"] 
+  	var cityNewYork = ["New York", "New York City", "NYC"];
+  	var citySanFran = ["San Francisco", "SF", "Bay Area"];
+	var cityLosAng = ["Los Angeles", "LA", "LAX"];
+	var cityAustin = ["Austin", "ATX"];
+	var citySydney = ["Sydney", "SYD"]; 
 
 // $("form").on(submit)
-	$('form').on('submit',function(e){
+	$('form').on('submit',function(e){ // User clicks submit 
 
 		e.preventDefault();
 
-		var city = $('#city-type').val();
+		var city = $('#city-type').val(); // what did the user type into the box?
 		
-		console.log(city);
-	})
+		console.log(city); //Get value of city
+	});
 
 	function 
 
 	
 	// $("p").css("background-image" url("")); -->make sure you go up and back down the directory so it pulls the image
+	// Use $.on(submit) or ```$.click``` to figure out when the user clicks the "submit" button   
+	$("#city-type").click(function() { 
+		$("cityNewYork").css(background-image , url("../images/nyc.jpg")); //Something from cityNewYork -> set the BG to nyc.jpg
+    	$("citySanFran").css(background-image , url("../images/sf.jpg")); //Something from citySanFran -> set the BG to sf.jpg
+		$("cityLosAng").css(background-image , url("../images/la.jpg")); //Something from cityLosAng -> set the BG to la.jpg
+		$("cityAustin").css(background-image , url("../images/austin.jpg")); //Something from citySydnet -> set the BG to sydney.jpg
+		$("citySydney").css(background-image , url("../images/sydney.jpg")); //Something from citySydnet -> set the BG to sydney.jpg
+		});
 
 
-	// | Use $.on(submit) or ```$.click``` to figure out when the user clicks the "submit" button   
-	$("#submit-btn").click(function() { 
-		$("cityNewYork").css("background-image" url("../images/nyc.jpg"));
-		// $("citySanFran").css('background-image url("../images/sf.jpg")');
-		// $("cityLosAng").css('background-image url("../images/la.jpg")');
-		// $("cityAustin").css('background-image url("../images/austin.jpg")');
-		// $("citySydney").css('background-image url("../images/sydney.jpg")');
-	// });
+	// IF/Else Method??
+	if city === "New York" or city === "New York City" or city = "NYC"
+		$("img").css("background-image: url("../images/nyc.jpg"));
 
-	});
+	if city === "San Fransisco" or city === "SF" or city = "Bay Area"
+		$("img").css("background-image: url("../images/sf.jpg"));
+
+	if city === "Los Angeles" or city === "LA" or city = "LAX"
+		$("img").css("background-image: url("../images/la.jpg"));
+
+	if city === "Austin" or city === "ATX" 
+		$("img").css("background-image: url("../images/austin.jpg"));
+
+	if city === "Sydney" or city === "SYD" 
+		$("img").css("background-image: url("../images/sydney.jpg"));
+	  
+});
+    
 
 	// $("#submit-btn").click(function() { 
 	// 	$("cityNewYork").css('background-image: "url(../images/nyc.jpg)');
